f0b6c6ae919046b9ad04f24706b240cf42ef7cc2
RaakshasHu <bakshiii2001@gmail.com>
2024-12-24 15:01:07 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,12 +1,1 @@
 
-
-            decoded_content = base64.b64decode(curprint("Successfully added a space to the file.")
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-
-        time.sleep(7200)
-
-if __name__ == "__main__":
-    main()
